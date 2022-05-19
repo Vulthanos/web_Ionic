ff6a3bbaabc2be2e9355bb126a6f37ec0290a0ff
Fernando Sanfiel Reyes <72128771+reyesanfer@users.noreply.github.com>
2022-05-19 18:21:06 +0100

    Reestructuración del proyecto con carpetas organizadas para interfaces, servicios, paginas, etc

---
+++ src/app/interfaces/producto.interface.ts
@@ -0,0 +1,6 @@
+export interface Producto{
+  description: string;
+  img: string;
+  name: string;
+  price: number;
+}
---
+++ src/app/services/products.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import {Producto} from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  productsCollection: AngularFirestoreCollection;
+
+  constructor(private af: AngularFirestore) {
+    this.productsCollection = this.af.collection<Producto>('products');
+  }
+
+  getProductos(){
+    return this.productsCollection.valueChanges({ idField: 'id' });
+  }
+}
