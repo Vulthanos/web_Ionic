ca399f87fb95b0091f7295fc8f904c9d06c80184
Fernando Sanfiel Reyes <72128771+reyesanfer@users.noreply.github.com>
2022-05-19 22:29:50 +0100

    Implementación de sistema de inicio de sesión y registro por firebase

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { UserAutenticationService } from '../../services/user-autentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  credentials: FormGroup;
 
-  ngOnInit() {
+  constructor(
+    private fb: FormBuilder,
+    private loadingController: LoadingController,
+    private alertController: AlertController,
+    private authService: UserAutenticationService,
+    private router: Router
+  ) { }
+
+  get email() {
+    return this.credentials.get('email');
   }
 
-  iniciarSesion(inicio: HTMLFormElement) {
+  get password() {
+    return this.credentials.get('password');
+  }
 
+  ngOnInit() {
+    this.credentials = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  async login() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+    const user = await this.authService.login(this.credentials.value);
+    await loading.dismiss();
+    if(user) {
+      await this.router.navigateByUrl('/home', {replaceUrl: true});
+    } else {
+      await this.showAlert('Inicio de sesión fallido', 'Vuelva a intentarlo');
+    }
+  }
+
+  async showAlert(header, message) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK'],
+    });
+    await alert.present();
   }
 }
